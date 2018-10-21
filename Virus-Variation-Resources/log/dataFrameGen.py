6d03a6e225d96e70592400ba6ecc9c26935b1138
tkong233 <tkong@wesleyan.edu>
2018-10-21 15:37:04 -0400

    data frame generate

--- Virus-Variation-Resources/log/dataFrameGen.py
+++ Virus-Variation-Resources/log/dataFrameGen.py
@@ -1,4 +1,4 @@
-from ecotypeUtils import *
+Gfrom ecotypeUtils import *
 from functools import reduce
 from itertools import groupby
 import pandas as pd
@@ -7,38 +7,40 @@ def flatten(l):
 	return [item for sublist in l for item in sublist]
 
 '''
-take in iterable(key, value)
-reduce by applying func to values with same key
+take in a log: a list of ecotypes, where each ecotype is a list of hosts
+return a list of distinct hosts paired with list of zeros correspond to number of ecotypes
 '''
-def reduceByKey(func, iterable):
-    get_first = lambda p: p[0]
-    get_second = lambda p: p[1]
-    return map(
-        lambda l: (l[0], reduce(func, map(get_second, l[1]))),
-        groupby(sorted(iterable, key=get_first), get_first)
-    )
+def host_init(log):
+	return [[host, [0]*len(log)] for host in list(set(flatten(log)))]
 
 '''
-take in ecotype: ['host', 'host', 'host']
-return [('host', 1), ('host', 1), ('host', 1)]
+take in a log,
+return a list of hosts paired with list of ints that count the host's occurrence in each ecotype
 '''
-def mapCounter(ecotype):
-	return [(host, 1) for host in ecotype]
+def countHost(log):
+	hosts = host_init(log)
+	for i, ecotype in enumerate(log):
+		for host_in_ecotype in ecotype:
+			for host in hosts:
+				if host_in_ecotype == host[0]:
+					host[1][i] += 1
+	return hosts
 
 
 Ecotypes = readLog()
 accession_host = readSource()
-newLog = mapAccessionHost(Ecotypes, accession_host)
-hosts = [(host, [0]*len(newLog)) for host in list(set(flatten(newLog)))]	# [('host', [0, 0, 0])]
 
+log = mapAccessionHost(Ecotypes, accession_host)
+countHost = [[i[0]]+i[1] for i in countHost(log)]
 
+col = ['Host']
+for i in range(len(log)):
+	col += ['Ecotype ' + str(i+1)]
 
-for i, ecotype in enumerate(newLog):
-	for host_in_ecotype in ecotype:
-		for host in hosts:
-			if host_in_ecotype == host[0]:
-				host[1][i] += 1
+df = pd.DataFrame(countHost, columns = col)
 
+filename = input('>name output csv file: ')
+df.to_csv(filename)
 
 
 
